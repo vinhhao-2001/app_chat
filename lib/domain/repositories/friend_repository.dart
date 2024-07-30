824686369450ec7bffde25cb27c33f497f5cab59
vinhhao <vinhhao1408@gmail.com>
2024-07-30 11:46:31 +0700

    repository complete

--- lib/domain/repositories/friend_repository.dart
+++ lib/domain/repositories/friend_repository.dart
@@ -1,7 +1,5 @@
-import 'package:app_chat/data/models/friend_model.dart';
 import 'package:app_chat/domain/entities/friend_entity.dart';
 
-abstract class FriendRepository{
+abstract class FriendRepository {
   Future<List<FriendEntity>> getFriendList(String token);
-
 }
